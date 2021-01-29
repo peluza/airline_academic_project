3cdbd1c8c9d41e8a7d7903b997358f41b3849cdd
peluza <edisonisaza@gmail.com>
2021-01-29 10:08:28 -0500

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,21 @@
-// this API consults the data base for airlines
+// This API consults the data base for airlines
 
-// add dependencies
+// Add dependencies
 const config = require("./controller/config.js");
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./views/route.js");
 const router_user = require("./views/route_users.js");
+const cors = require("cors");
 
+// Excute the express
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Add routes
 app.use("/api/v1", router);
 app.use("/users", router_user);
 app.listen(config.port, () =>
